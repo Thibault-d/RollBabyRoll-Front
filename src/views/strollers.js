371e356d91f7f00de35d3c4a5a6545006d41eaf6
Thibault-d <thibaultdelfaud@gmail.com>
2020-05-11 11:00:26 +0200

    refactored filters, adding another one

--- src/views/strollers.js
+++ src/views/strollers.js
@@ -3,9 +3,10 @@ import apiClient from "../services/Strollers.js";
 
 export default class Strollers extends Component {
   state = {
-    filters: [null, null, null],
     allStrollers: [],
     filteredStrollers: null,
+    priceFilters: [null, null, null],
+    birthFilter: [null, null],
   };
 
   loadStrollers() {
@@ -26,52 +27,34 @@ export default class Strollers extends Component {
   }
 
   buttonClickHandler = (e) => {
-    let id = e.target.id;
-    let buttonStatus = this.state.filters;
-    buttonStatus[id] === id
+    let { id, name } = e.target;
+    let buttonStatus = this.state.priceFilters;
+    buttonStatus[id] === name
       ? (buttonStatus[id] = null)
-      : (buttonStatus[id] = id);
+      : (buttonStatus[id] = name);
     this.forceUpdate();
-    this.filter()
+    this.filter();
   };
 
-  buttonColorHandler = (id) => {
+  buttonColorHandler = (id, name) => {
     let string = "";
-    let buttonStatus = this.state.filters;
-    buttonStatus[id] === id
+    let buttonStatus = this.state.priceFilters;
+    buttonStatus[id] === name
       ? (string = "ActiveButton")
       : (string = "InactiveButton");
     return string;
   };
 
-  filterConstruction = () => {
-    let { filters } = this.state;
-    let clean = [];
-    let a = 0;
-    filters.map((item, index) => {
-      if (filters[index] === "0") {
-        clean[a] = "€";
-      } else if (filters[index] === "1") {
-        clean[a] = "€€";
-      } else if (filters[index] === "2") {
-        clean[a] = "€€€";
-      }
-      a++;
-      return clean;
-    });
-    return clean;
-  };
-
   filter = () => {
-    let { allStrollers } = this.state;
-    let filterArray = this.filterConstruction();
-    let essai = null;
-    essai = filterArray.map((item, index) =>
-      allStrollers.filter((item) => item.pricerange === filterArray[index])
+    let { allStrollers, priceFilters } = this.state;
+    let filterResult = null;
+    if(priceFilters ){console.log("not null")} else {console.log("null")}
+    filterResult = priceFilters.map((item, index) =>
+      allStrollers.filter((item) => item.pricerange === priceFilters[index])
     );
     this.setState({
-      filteredStrollers: essai.flat()
-    })
+      filteredStrollers: filterResult.flat(),
+    });
   };
 
   renderStrollers = () => {
@@ -113,49 +96,68 @@ export default class Strollers extends Component {
             <input
               type="button"
               id="0"
-              className={this.buttonColorHandler("0")}
+              name="€"
+              className={this.buttonColorHandler("0", "€")}
               value="€"
               onClick={this.buttonClickHandler}
             />
             <input
               type="button"
               id="1"
-              className={this.buttonColorHandler("1")}
+              className={this.buttonColorHandler("1", "€€")}
+              name="€€"
               value="€€"
               onClick={this.buttonClickHandler}
             />
             <input
               type="button"
               id="2"
-              className={this.buttonColorHandler("2")}
+              className={this.buttonColorHandler("2", "€€€")}
+              name="€€€"
               value="€€€"
               onClick={this.buttonClickHandler}
             />
           </div>
-          <div className="Weight-filter">
-             Maximum weight
+          <div className="Price-filter">
+            <h2>Can be used from birth?</h2>
+            <input
+              type="button"
+              id="3"
+              name="yes"
+              className={this.buttonColorHandler("3", "yes")}
+              value="yes"
+              onClick={this.buttonClickHandler}
+            />
+            <input
+              type="button"
+              id="4"
+              className={this.buttonColorHandler("4", "no")}
+              name="no"
+              value="no"
+              onClick={this.buttonClickHandler}
+            />
           </div>
-        </div>
-        <div className="Stroller-container">
-          <div>
-            <div className="Stroller-img">Image</div>
-            <div>Model</div>
-            <div>Brand </div>
-            <div>Storage</div>
-            <div>Handle</div>
-            <div>Dimension folded </div>
-            <div>Dimension open </div>
-            <div>Maximum Weight </div>
-            <div>Weight </div>
-            <div>Brakes </div>
-            <div>Reversible</div>
-            <div>Use from birth</div>
-            <div>Use for sport </div>
-            <div>Double </div>
-            <div>Price range</div>
-            <div>Suspensions</div>
+          <div className="Stroller-container">
+            <div>
+              <div className="Stroller-img">Image</div>
+              <div>Model</div>
+              <div>Brand </div>
+              <div>Storage</div>
+              <div>Handle</div>
+              <div>Dimension folded </div>
+              <div>Dimension open </div>
+              <div>Maximum Weight </div>
+              <div>Weight </div>
+              <div>Brakes </div>
+              <div>Reversible</div>
+              <div>Use from birth</div>
+              <div>Use for sport </div>
+              <div>Double </div>
+              <div>Price range</div>
+              <div>Suspensions</div>
+            </div>
+            {this.renderStrollers()}
           </div>
-          {this.renderStrollers()}
         </div>
       </div>
     );
